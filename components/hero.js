1af0464f879e318fb77711c45c59f0d9a7d640a4
John Bentley <jwbentle@gmail.com>
2020-07-16 20:56:59 -0700

    Tidy up styles on tablet and desktop view

--- components/hero.js
+++ components/hero.js
@@ -3,17 +3,17 @@ import Link from 'next/link'
 
 export default function Hero() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <div className={`hero__image-container sm:hidden md:inline`}>
-        <img className={`sm:hidden md:inline`} src={'https://i2.wp.com/rachelwanghere.com/wp-content/uploads/2020/06/Featured-Photo-320x321-1.png?fit=320%2C321&ssl=1'} />
+    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12 sm:p-0 md:px-12">
+      <div className={`hero__image-container sm:hidden md:inline w-full md:w-3/4`}>
+        <img className={`hidden md:inline`} src={'https://i2.wp.com/rachelwanghere.com/wp-content/uploads/2020/06/Featured-Photo-320x321-1.png?fit=320%2C321&ssl=1'} />
       </div>
-      <div className={`hero__text-container`}>
-        <h1 className="hero__heading text-6xl md:text-8xl font-bold tracking-tighter leading-tight pt-6 pb-2 pr-8">
+      <div className={`hero__text-container w-full`}>
+        <h1 className={`hero__heading font-bold tracking-tighter leading-tight text-6xl md:text-7xl pt-6 pb-2 pr-8 w-full`}>
           Join and get access to my workbook!
         </h1>  
-        <p>Lorem ipsum solor dut amet</p>    
+        <p className={`hero__subtitle mb-10`}>Lorem ipsum solor dut amet</p>    
         <Link href="/">
-          <a>Register Now</a>
+          <a className={`hero__cta-button bg-gray-600 text-white cursor-pointer text-lg px-10 py-4 rounded`}>Register Now</a>
         </Link>
       </div>
     </section>
--- components/image-text-split.js
+++ components/image-text-split.js
@@ -2,28 +2,30 @@ import Link from 'next/link'
 
 export default function ImageTextSplit() {
   return (
-    <section className="image-text-split align-center my-32">
-      <h2 className={`image-text-split__heading text-2xl text-center mb-6`}>Hi, I'm Rachel</h2>
-      <div className={`image-text-split__sample-image bg-gray-400 m-auto w-40 h-40 mb-6`}>
+    <section className="image-text-split align-center my-32 md:flex md:px-12">
+      <div className={`image-text-split__text-container w-6/12 text-left md:flex-initial md:pr-32`}>
+        <h2 className={`image-text-split__heading text-5xl mb-6`}>Hi, I'm Rachel</h2>
+        <p className={`image-text-split__text`}>
+          Lorem ipsum dolor sit amet, consectetur 
+          adipiscing elit. Phasellus ultricies ultricies 
+          augue eu feugiat. Orci varius natoque 
+          penatibus et magnis dis parturient montes, 
+          nascetur ridiculus mus. Sed vel lectus arcu. 
+
+          Vestibulum ante ipsum primis in faucibus 
+          orci luctus et ultrices posuere cubilia curae; 
+          Duis eleifend non lorem vel pulvinar. Proin 
+          in pretium nisi. 
+
+          In mollis, elit tristique pretium pharetra, 
+          tortor ullamcorper odio. Suspendisse 
+          posuere massa quis ipsum egestas, a 
+          ullamcorper libero faucibus. I
+        </p>
+      </div>
+      <div className={`image-text-split__image-container m-auto w-40 md:w-4/12 h-40 mb-6 md:flex-initial md:ml-12`}>
         <img src={'https://i2.wp.com/rachelwanghere.com/wp-content/uploads/2020/06/Featured-Photo-320x321-1.png?fit=320%2C321&ssl=1'} />
       </div>
-      <p className={`image-text-split__text`}>
-        Lorem ipsum dolor sit amet, consectetur 
-        adipiscing elit. Phasellus ultricies ultricies 
-        augue eu feugiat. Orci varius natoque 
-        penatibus et magnis dis parturient montes, 
-        nascetur ridiculus mus. Sed vel lectus arcu. 
-
-        Vestibulum ante ipsum primis in faucibus 
-        orci luctus et ultrices posuere cubilia curae; 
-        Duis eleifend non lorem vel pulvinar. Proin 
-        in pretium nisi. 
-
-        In mollis, elit tristique pretium pharetra, 
-        tortor ullamcorper odio. Suspendisse 
-        posuere massa quis ipsum egestas, a 
-        ullamcorper libero faucibus. I
-      </p>
     </section>
   )
 }
