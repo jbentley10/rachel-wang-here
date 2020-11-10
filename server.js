d54d4d622f88a1c2b78221b63a496237328aa658
John Bentley <jwbentle@gmail.com>
2020-11-09 20:48:38 -0800

    Got WooCommerce to display data

--- server.js
+++ server.js
@@ -1,48 +1,56 @@
-// server.js
 const next = require('next');
 const express = require('express');
-const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
 const wooConfig = require('./wooConfig');
 
-const wooCommerceAPI = new WooCommerceRestApi({
-  url: wooConfig.siteURL,
+const  WooCommerceAPI = require("@woocommerce/woocommerce-rest-api").default;
+
+const WooCommerce = new WooCommerceAPI({
+	url: wooConfig.siteURL,
   consumerKey: wooConfig.consumerKey,
   consumerSecret: wooConfig.consumerSecret,
   version: "wc/v3"
 });
 
-const PORT = 3002;
+const port = 3002;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare()
-  .then(() => {
+	.then(() => {
     const server = express();
-    server.get('*', (req, res) => {
-      return handle(req, res);
+    
+    server.get('/', (req, response) => {
+      console.log("hello");
     });
- 
-    server.get('/getProducts', (req, res) => {
-      wooCommerceAPI.get("products")
+
+    server.get('/getProducts', (req, response) => {
+      WooCommerce.get("products", {
+        per_page: 20, // 20 products per page
+      })
         .then((response) => {
-          console.log(response.data);
+          // Successful request
+          console.log("Response Status:", response.status);
+          console.log("Response Headers:", response.headers);
+          console.log("Response Data:", response.data);
+          console.log("Total of pages:", response.headers['x-wp-totalpages']);
+          console.log("Total of items:", response.headers['x-wp-total']);       
         })
         .catch((error) => {
-          console.log(error.response.data);
+          // Invalid request, for 4xx and 5xx statuses
+          console.log("Response Status:", error.response.status);
+          console.log("Response Headers:", error.response.headers);
+          console.log("Response Data:", error.response.data);
+        })
+        .finally(() => {
+          // Always executed.
         });
-    })
-
-    server.listen(PORT, err => {
-      if (err) {
-        throw err;
-      }
-
-      console.log("Ready on port " + PORT);
-    })
-  })
-
-  .catch(ex => {
-    console.error(ex.stack);
-    process.exit(1);
+    });
+    
+    server.listen(port, err => {
+			if ( err ) {
+				throw err;
+			}
+			console.log(`Ready on ${port}`);
+		})
   });
