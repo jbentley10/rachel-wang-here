47d85e40d57727002dea8ae4b7e9750703910442
John Bentley <jwbentle@gmail.com>
2020-12-08 21:33:46 -0800

    Start implementing wave bg for three column icons

--- components/hero.js
+++ components/hero.js
@@ -7,7 +7,7 @@ import Image from 'next/image';
 
 export default function Hero() {
   return (
-    <section className="flex-col md:flex-row md:block lg:flex items-center md:justify-between md:mb-12 px-10 py-16 md:px-24 lg:px-64">
+    <section className="flex-col md:flex-row md:block lg:flex items-center md:justify-between px-10 py-16 md:px-24 lg:px-64">
       <div className={`hero__text-container w-full relative md:block lg:inline z-20 lg:w-1/2`}>
         <div className={`clear-background bg-clear-background p-10 relative h-full leading-10 transform translate-x-4 translate-y-4`}>
           <div className={`neutral-text-box bg-neutral-background p-10 leading-10`}>
--- components/three-column-split.js
+++ components/three-column-split.js
@@ -1,39 +1,129 @@
-import Link from 'next/link'
+/** 
+ * @file three-column-split.js
+ */
+
+// Import dependencies
+import Image from 'next/image';
 
 export default function ThreeColumnSplit() {
   return (
-    <section className="three-column-split text-center align-center md:px-64 md:py-24">
-      <div className={`three-column-split__heading-container pt-12 md:pt-24`}>
-        <h2 className={`text-6xl md:text-7xl font-raleway`}>Take Action!</h2>
+    <section className="three-column-split text-center align-center">
+      <div className={`wavy-background w-full h-8`}>
+        <div id={`wave`}>
+          <svg class="waves" viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
+            <defs>
+              <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+            </defs>
+            <g class="parallax">
+              <use x="48" y="0" fill="rgba(255,255,255,0.7" />
+              <use x="48" y="3" fill="rgba(255,255,255,0.5)" />
+              <use x="48" y="5" fill="rgba(255,255,255,0.3)" />
+              <use x="48" y="7" fill="#fff" />
+            </g>
+          </svg>
+        </div>
       </div>
-      <div className={`three-column-split__links-container`}>
-        <ul className={`three-column-split__links-list sm:inline lg:flex w-full`}>
-          <Link href="/">
-            <li className={`three-column-split__column lg:flex-initial w-full my-12`}>
-              <div className={`three-column-split__image-container text-center rounded-lg m-auto w-56 h-auto bg-sedona opacity-100 py-8`}>
-                <img className={`opacity-100 w-1/3 m-auto text-center`} src={`../static/SVG/yoga-mat.svg`} />
-              </div>
-              <h3 className={`three-column-split__text text-xl md:text-2xl pt-4 md:pt-10 font-raleway`}>Get Free Resources</h3>
-            </li>
-          </Link>
-          <Link href="/">
-            <li className={`three-column-split__column lg:flex-initial w-full my-12`}>
-              <div className={`three-column-split__image-container text-center rounded-lg m-auto w-56 h-auto bg-sedona opacity-100 py-8`}>
-                <img className={`opacity-100 w-1/3 m-auto text-center`} src={`../static/SVG/yoga-mat.svg`} />
-              </div>
-              <h3 className={`three-column-split__text text-xl md:text-2xl pt-4 md:pt-10 font-raleway`}>Livestream Classes</h3>
-            </li>
-          </Link>
-          <Link href="/">
-            <li className={`three-column-split__column lg:flex-initial w-full my-12`}>
-              <div className={`three-column-split__image-container text-center rounded-lg m-auto w-56 h-auto bg-sedona opacity-100 py-8`}>
-                <img className={`opacity-100 w-1/3 m-auto text-center`} src={`../static/SVG/yoga-mat.svg`} />
-              </div>
-              <h3 className={`three-column-split__text text-xl md:text-2xl pt-4 md:pt-10 font-raleway`}>Join Community</h3>
-            </li>
-          </Link>
-        </ul>
-      </div>
+
+      <style jsx>{`
+        @import url(//fonts.googleapis.com/css?family=Lato:300:400);
+        body {
+          margin: 0;
+        }
+        h1 {
+          font-family: 'Lato', sans-serif;
+          font-weight: 300;
+          letter-spacing: 2px;
+          font-size: 48px;
+        }
+        p {
+          font-family: 'Lato', sans-serif;
+          letter-spacing: 1px;
+          font-size: 14px;
+          color: #333333;
+        }
+        .header {
+          position: relative;
+          text-align: center;
+          background: linear-gradient(60deg, rgba(84, 58, 183, 1) 0%, rgba(0, 172, 193, 1) 100%);
+          color: white;
+        }
+        .logo {
+          width: 50px;
+          fill: white;
+          padding-right: 15px;
+          display: inline-block;
+          vertical-align: middle;
+        }
+        .inner-header {
+          height: 65vh;
+          width: 100%;
+          margin: 0;
+          padding: 0;
+        }
+        .flex {
+          /*Flexbox for containers*/
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          text-align: center;
+        }
+        .waves {
+            position: relative;
+            width: 100%;
+            height: 15vh;
+            margin-bottom: -7px;
+            /*Fix for safari gap*/
+            min-height: 100px;
+            max-height: 150px;
+        }
+        .content {
+            position: relative;
+            height: 20vh;
+            text-align: center;
+            background-color: white;
+        }
+        /* Animation */
+        .parallax>use {
+            animation: move-forever 25s cubic-bezier(.55, .5, .45, .5) infinite;
+        }
+        .parallax>use:nth-child(1) {
+            animation-delay: -2s;
+            animation-duration: 7s;
+        }
+        .parallax>use:nth-child(2) {
+            animation-delay: -3s;
+            animation-duration: 10s;
+        }
+        .parallax>use:nth-child(3) {
+            animation-delay: -4s;
+            animation-duration: 13s;
+        }
+        .parallax>use:nth-child(4) {
+            animation-delay: -5s;
+            animation-duration: 20s;
+        }
+        @keyframes move-forever {
+            0% {
+                transform: translate3d(-90px, 0, 0);
+            }
+            100% {
+                transform: translate3d(85px, 0, 0);
+            }
+        }
+        /*Shrinking for mobile*/
+        @media (max-width: 768px) {
+            .waves {
+                height: 40px;
+                min-height: 40px;
+            }
+            .content {
+                height: 30vh;
+            }
+            h1 {
+                font-size: 24px;
+            }
+        }
+      `}</style>
     </section>
   )
 }
