3aa87d215e07b6b6747ab98f6274af3f8f3fd779
John Bentley <jwbentle@gmail.com>
2020-12-26 20:20:56 -0800

    Fix about page heading styles

--- components/overlay-text-box.js
+++ components/overlay-text-box.js
@@ -5,14 +5,14 @@
 
 export default function OverlayTextBox({ text1, text2 }) {
   return (
-    <section className={`overlay-text-box bg-purple w-3/4 md:w-5/12 p-12 m-auto md:ml-32 md:-mt-48`}>
-      <p className={`text-white font-barlow`}>
+    <section className={`overlay-text-box bg-purple w-3/4 md:w-5/12 p-12 m-auto md:ml-32 md:-mt-48 text-white font-barlow`}>
+      <p>
         <div dangerouslySetInnerHTML={{ __html: text1 }} />  
       </p>      
       <span className={`text-h2 text-white m-auto text-center font-barlow`}>. . . .</span>
       <br />
       <br />
-      <p className={`text-white font-barlow`}>
+      <p>
         <div dangerouslySetInnerHTML={{ __html: text2 }} />
       </p>
     </section>
