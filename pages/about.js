af0bf09820a56e0f0264edd0d3d6830b07b30bc5
John Bentley <jwbentle@gmail.com>
2021-01-17 19:21:00 -0800

    Finish adding heading images in Contenful

--- pages/about.js
+++ pages/about.js
@@ -20,51 +20,52 @@ import { getAllPostsForHome } from '../lib/api'
 import PopoutBlade from '../components/popout-blade';
 import PopoutBladeAlt from '../components/popout-blade-alt';
 
-export default function About({ preview, aboutContent, sidebarContent, footerContent, posts: { edges } }) {
+export default function About({ preview, pageContent, sidebarContent, footerContent, posts: { edges } }) {
   const recentPosts = edges.slice(0, 3);
   return (
     <div>
       {/* Meta description for SEO */}
-      <Layout footerContent={footerContent} metaDescription={aboutContent.fields.metaDescription} preview={preview}>
+      <Layout footerContent={footerContent} metaDescription={pageContent.fields.metaDescription} preview={preview}>
         <Head>
           {/* Title tag for SEO */}
-          <title>{ aboutContent.fields.pageTitle }</title>
+          <title>{ pageContent.fields.pageTitle }</title>
         </Head>
         <Container>
           <Header />
           <HeroSplitRight 
-            heading={aboutContent.fields.pageHeading}
+            heading={pageContent.fields.pageHeading}
+            image={pageContent.fields.pageHeadingImage.fields.file.url}
           />
           <OverlayTextBox 
-            text1={documentToHtmlString(aboutContent.fields.purpleBoxCopy1)}
-            text2={documentToHtmlString(aboutContent.fields.purpleBoxCopy2)}
+            text1={documentToHtmlString(pageContent.fields.purpleBoxCopy1)}
+            text2={documentToHtmlString(pageContent.fields.purpleBoxCopy2)}
           />
           <div className={`page-body-content px-12 md:px-32 lg:px-64 bg-side-blobs-combined--purple bg-contain bg-no-repeat`}>
             <div className={`sidebar-body-split flex`}>
               <div className={`text-block-layout-container flex-initial md:w-7/12 pr-16 mt-24`}>
                 <h1 className={`highlighted-text text-h1 font-rylan text-text-color leading-none`}>
-                  {aboutContent.fields.highlightedHeading}
+                  {pageContent.fields.highlightedHeading}
                 </h1>
                 <div className={`copy-block my-24`}>
-                  <h2 className={`font-rylan text-h2 text-text-color leading-none`}>{aboutContent.fields.section1Heading}</h2>
+                  <h2 className={`font-rylan text-h2 text-text-color leading-none`}>{pageContent.fields.section1Heading}</h2>
                   <p className={`text-paragraph leading-normal font-barlow text-text-color`}>
-                    <div dangerouslySetInnerHTML={{ __html: documentToHtmlString(aboutContent.fields.section1Copy) }} />
+                    <div dangerouslySetInnerHTML={{ __html: documentToHtmlString(pageContent.fields.section1Copy) }} />
                   </p>
                 </div>
                 <div className={`image-copy-block my-24`}>
                   <div className={`clear-background bg-clear-background p-10 relative z-10 transform translate-x-8 translate-y-24 h-full`} />
                   <div className={`transform -translate-y-32 relative z-30`}>
-                    <img src={`https:` + aboutContent.fields.section1Image.fields.file.url} />
+                    <img src={`https:` + pageContent.fields.section1Image.fields.file.url} />
                   </div>
-                  <h2 className={`font-rylan text-h2 text-text-color`}>{aboutContent.fields.section2Heading}</h2>
+                  <h2 className={`font-rylan text-h2 text-text-color`}>{pageContent.fields.section2Heading}</h2>
                   <div className={`text-paragraph leading-normal font-barlow text-text-color`}>
-                    <div dangerouslySetInnerHTML={{ __html: documentToHtmlString(aboutContent.fields.section2Copy) }} />
+                    <div dangerouslySetInnerHTML={{ __html: documentToHtmlString(pageContent.fields.section2Copy) }} />
                   </div>
                 </div>
                 <div className={`copy-block my-24`}>
-                  <h2 className={`font-rylan text-h2 text-text-color`}>{aboutContent.fields.section3Heading}</h2>
+                  <h2 className={`font-rylan text-h2 text-text-color`}>{pageContent.fields.section3Heading}</h2>
                   <div className={`text-paragraph leading-normal font-barlow text-text-color`}>
-                    <div dangerouslySetInnerHTML={{ __html: documentToHtmlString(aboutContent.fields.section3Copy) }} />
+                    <div dangerouslySetInnerHTML={{ __html: documentToHtmlString(pageContent.fields.section3Copy) }} />
                   </div>
                 </div>
               </div>
@@ -74,16 +75,16 @@ export default function About({ preview, aboutContent, sidebarContent, footerCon
             </div>
           </div>
           <PopoutBlade 
-            heading={aboutContent.fields.popoutBlade1Heading}
-            body={documentToHtmlString(aboutContent.fields.popoutBlade1Copy)}
+            heading={pageContent.fields.popoutBlade1Heading}
+            body={documentToHtmlString(pageContent.fields.popoutBlade1Copy)}
           />
           <div className={`bg-wavy-background w-full h-64`} />
           <PopoutBladeAlt
-            heading={aboutContent.fields.popoutBlade2Heading}
-            body={documentToHtmlString(aboutContent.fields.popoutBlade2Copy)}
+            heading={pageContent.fields.popoutBlade2Heading}
+            body={documentToHtmlString(pageContent.fields.popoutBlade2Copy)}
             button={{
-              href: aboutContent.fields.popoutBlade2ButtonLink,
-              text: aboutContent.fields.popoutBlade2ButtonText,
+              href: pageContent.fields.popoutBlade2ButtonLink,
+              text: pageContent.fields.popoutBlade2ButtonText,
               color: 'purple'
             }}
           />
@@ -103,14 +104,14 @@ export default function About({ preview, aboutContent, sidebarContent, footerCon
 export async function getStaticProps({ preview = false }) {
   const posts = await getAllPostsForHome(preview);
   const sidebarContent = await fetchSidebar();  
-  const aboutContent = await fetchAbout();
+  const pageContent = await fetchAbout();
   const footerContent = await fetchFooter();
 
-  if (sidebarContent.fields && aboutContent.fields && footerContent.fields) {
+  if (sidebarContent.fields && pageContent.fields && footerContent.fields) {
     return {
       props: {
         sidebarContent,
-        aboutContent,
+        pageContent,
         footerContent,
         posts,
         preview
